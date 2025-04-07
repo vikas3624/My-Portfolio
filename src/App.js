45deac32c7acf7230bf832937c4121c72b845628
Sai Vikas, Goli <saivicky9@gmail.com>
2025-04-07 10:20:02 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Sai Vikas Goli</h1>
+        <p>Full Stack Developer</p>
       </header>
     </div>
   );
